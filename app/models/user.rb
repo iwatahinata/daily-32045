174481198b06040b485fbc0e4ee9cce9913eed43
iwatahinata <iwata.10120612@gmail.com>
2020-12-02 11:41:57 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,7 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください'
+
+
+
 end
