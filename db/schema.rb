df444de9150ffac306e8ef99cdd8d1fbb5748ace
iwatahinata <iwata.10120612@gmail.com>
2020-12-04 14:23:40 +0900

    カート機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2020_12_03_023105) do
     t.string "nickname", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
+    t.integer "type_id", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
