6814a0c027d9b924d3c5cbd40214551284fd479a
iwatahinata <iwata.10120612@gmail.com>
2020-12-07 18:39:41 +0900

    slideの修正

--- app/javascript/slick.js
+++ app/javascript/slick.js
@@ -1,7 +1,11 @@
-$(document).on('turbolinks:load', function(){
-  $('.slick').slick({
-    autoplay:true,  
-    dots:true,      
+$(function(){
+  $("#slider").slick({
+    speed: 1000,
+    dots: true,
+    dotsClass: 'slide-dots',
+    autoplay: true,
+    fade: true
   });
 });
+
 
