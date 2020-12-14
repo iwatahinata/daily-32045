313eaa1bfa137e0ec51d8307029140806daf1756
iwatahinata <iwata.10120612@gmail.com>
2020-12-14 17:06:22 +0900

    お気に入り機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "foods#index"
+  post "favorites/:food_id/destroy" => "favorites#destroy"
+  post "favorites/:food_id/create" => "favorites#create"
    resources :foods do
     resources :orders, only: [:create, :index]
+    resource :favorites, only: [:create, :destroy]
     collection do
       get 'search'
     end
