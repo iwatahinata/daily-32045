1a84432dacc9dbdb03f76bd091ad0ba67cd4c487
iwatahinata <iwata.10120612@gmail.com>
2020-12-15 17:55:23 +0900

    購入機能のテストコード修正

--- db/migrate/20201203022829_create_addresses.rb
+++ db/migrate/20201203022829_create_addresses.rb
@@ -3,7 +3,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
     create_table :addresses do |t|
       t.string     :postal_code,     null: false
       t.string     :city,            null: false
-      t.string     :street,  　　　　　null: false
+      t.string     :street,          null: false
       t.string     :house_name
       t.string     :phone_number,    null: false
       t.references :user,            foreign_key: true
